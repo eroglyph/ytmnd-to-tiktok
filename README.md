f488dee3817d0ab69c01ad9a6c67b4d416db547b
Philip Bjerknes <philleif@Philips-iMac-Pro.local>
2019-01-05 12:50:15 -0500

    omnibus updates

--- README.md
+++ README.md
@@ -1,10 +1,97 @@
 # ytmnd-to-tiktok
 Convert YTMND sites into videos for TikTok.
 
-# ffmpeg
-ffmpeg -i timetraveller/image.gif -f mp4 -pix_fmt yuv420p timetraveller/video.mp4
+# Specs
 
-ffmpeg -r 24 -i sites/timetraveller/image.gif -vcodec libx264 -y -an sites/timetraveller/video.mp4 -vf "pad=ceil(iw/2)*2:ceil(ih/2)*2" -pix_fmt yuv420p
+Video Size: 375x812px
 
-ffmpeg -i sites/timetraveller/image.gif -f mp4 -pix_fmt yuv420p sites/timetraveller/video.mp4 -y
+convert sites/hippohippo/stacked_image.gif -resize 375x375 sites/hippohippo/resized_image.gif
+
+
+# Tiled Foreground (http://steamsteamlol.ytmnd.com/)
+
+convert -size 375x812 xc:#000000 sites/steamsteamlol/background.png && convert sites/steamsteamlol/image.gif -resize 375x375 sites/steamsteamlol/image_resized.gif
+
+sox sites/steamsteamlol/sound.wav sites/steamsteamlol/sound_looped.mp3 repeat 20 && ffmpeg -ss 0 -t 60 -i sites/steamsteamlol/sound_looped.mp3 sites/steamsteamlol/sound_final.mp3 && rm sites/steamsteamlol/sound_looped.mp3
+
+ffmpeg -i sites/hippohippo/image.gif -i sites/hippohippo/image.gif -i sites/hippohippo/image.gif -filter_complex vstack=inputs=3 sites/hippohippo/stacked_image.gif
+
+ffmpeg -i assets/bg0.mp4 -i sites/steamsteamlol/background.png \
+-filter_complex "[0:v][1:v] overlay=0:0" \
+-pix_fmt yuv420p -c:a copy \
+sites/steamsteamlol/video_background.mp4
+
+ffmpeg -i assets/bg.mp4 -ignore_loop 0 -i sites/hippohippo/image_stacked.gif -filter_complex "[0:v][1:v] overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2:enable='between(t,0,60):shortest=1" -pix_fmt yuv420p -c:a copy sites/hippohippo/video.mp4
+
+ffmpeg -i sites/steamsteamlol/video.mp4 -i sites/steamsteamlol/sound_final.mp3  -filter_complex "[1:0] adelay=0|0 [delayed];[0:1][delayed] amix=inputs=2" -map 0:0 -c:a aac -strict -2 -c:v copy sites/steamsteamlol/tiktok.mp4
+
+# Image w/Color Background (http://ualuealuealeuale.ytmnd.com/)
+
+Generate background:
+convert -size 375x812 xc:#000000 sites/ualuealuealeuale/background.png
+
+Resize foreground image:
+convert sites/ualuealuealeuale/image.gif -resize 375x375 sites/ualuealuealeuale/image_resized.gif
+
+Create 60s audio:
+sox sites/ualuealuealeuale/sound.wav sites/ualuealuealeuale/sound_looped.mp3 repeat 20 && ffmpeg -ss 0 -t 60 -i sites/ualuealuealeuale/sound_looped.mp3 sites/ualuealuealeuale/sound_final.mp3 && rm sites/ualuealuealeuale/sound_looped.mp3
+
+Generate video:
+ffmpeg -i assets/bg0.mp4 -i sites/ualuealuealeuale/background.png \
+-filter_complex "[0:v][1:v] overlay=0:0" \
+-pix_fmt yuv420p -c:a copy \
+sites/ualuealuealeuale/video_background.mp4 \
+&& \
+ffmpeg -i sites/ualuealuealeuale/video_background.mp4 -ignore_loop 0 -i sites/ualuealuealeuale/image_resized.gif \
+-filter_complex "[0:v][1:v] overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2:enable='between(t,0,60):shortest=1" \
+-pix_fmt yuv420p -c:a copy \
+sites/ualuealuealeuale/video.mp4
+
+Add sound:
+ffmpeg -i sites/ualuealuealeuale/video.mp4 \
+  -i sites/ualuealuealeuale/sound_final.mp3 \
+  -filter_complex "[1:0] adelay=0|0 [delayed];[0:1][delayed] amix=inputs=2" \
+  -map 0:0 \
+  -c:a aac -strict -2 \
+  -c:v copy \
+  sites/ualuealuealeuale/tiktok.mp4
+
+# Media Formatting Reference
+
+Create background:
+
+convert -size 375x812 xc:#000000 sites/ualuealuealeuale/background.png
+
+Loop short audio:
+
+sox sites/steamsteamlol/sound.wav sites/steamsteamlol/sound_full.wav repeat 20
+
+Clip long audio:
+
+ffmpeg -ss 0 -t 60 -i sites/steamsteamlol/sound_full.wav sites/pi/sound_final.wav
+
+Composite sound and video:
+
+ffmpeg -i sites/steamsteamlol/video.mp4 \
+  -i sites/steamsteamlol/sound_final.wav \
+  -filter_complex "[1:0] adelay=0|0 [delayed];[0:1][delayed] amix=inputs=2" \
+  -map 0:0 \
+  -c:a aac -strict -2 \
+  -c:v copy \
+  sites/steamsteamlol/tiktok.mp4
+
+Resize image to fit width:
+
+convert sites/pi/image.jpg -resize 375x375 sites/pi/resize_image.jpg
+
+Static image:
+
+ffmpeg -i assets/bg0.mp4 -i sites/pi/resize_image.jpg -filter_complex "[0:v][1:v] overlay=0:0" -pix_fmt yuv420p -c:a copy sites/pi/video.mp4
+
+GIF:
+
+ffmpeg -i assets/bg0.mp4 -ignore_loop 0 -i sites/steamsteamlol/image.gif \
+-filter_complex "[0:v][1:v] overlay=0:0:enable='between(t,0,60):shortest=1" \
+-pix_fmt yuv420p -c:a copy \
+sites/steamsteamlol/video.mp4
 
--- bin/ytmnd.js
+++ bin/ytmnd.js
@@ -3,10 +3,25 @@
 const axios = require("axios")
 const mkdirp = require("mkdirp")
 const wget = require("node-wget")
-const { getAudioDurationInSeconds } = require("get-audio-duration")
+const cmd = require("node-cmd")
+const path = require("path")
 
-const ytmnd = "pi"
+const { getAudioDurationInSeconds } = require("get-audio-duration")
+const commandLineArgs = require("command-line-args")
+
+const optionDefinitions = [
+  { name: "ytmnd", type: String, multiple: false, defaultOption: true },
+  { name: "length", type: String, multiple: false, defaultOption: false }
+]
+
+const options = commandLineArgs(optionDefinitions)
+
+console.log(options)
+
+const ytmnd = options.ytmnd
 const ytmndUrl = "http://" + ytmnd + ".ytmnd.com"
+const ytmndPath = "./sites/" + ytmnd + "/"
+let ytmndInfo = {}
 
 // get ytmnd
 const fetchHtml = async () => {
@@ -29,6 +44,10 @@ const getData = async html => {
     let res = html.match(/(\/info.*json)/gi)
     let jsonUrl = ytmndUrl + res[0]
 
+    console.log(jsonUrl)
+
+    download(jsonUrl, "info.json") // archive this
+
     axios
       .get(jsonUrl)
       .then(function(response) {
@@ -46,7 +65,7 @@ const download = async (url, filename) => {
     wget(
       {
         url: url,
-        dest: "./sites/" + ytmnd + "/" + filename
+        dest: ytmndPath + filename
       },
       function(error, response, body) {
         if (error) {
@@ -59,25 +78,85 @@ const download = async (url, filename) => {
   })
 }
 
-const run = async () => {
-  let html = await fetchHtml()
-  let data = await getData(html)
-  let imageFilename = data.site.foreground.url.replace(/(.*)\./gi, "image.")
-  let soundFilename = data.site.sound.url.replace(/(.*)\./gi, "sound.")
-
-  // create folder, do nothing if it already exists
-  mkdirp("./sites/" + ytmnd, function(error) {
-    console.log(error)
-  })
-
-  await download(data.site.foreground.url, imageFilename)
-  await download(data.site.sound.url, soundFilename)
-
-  getAudioDurationInSeconds("./sites/" + ytmnd + "/" + soundFilename).then(
-    duration => {
-      console.log(duration + "seconds")
+// get audio file duration
+const getDuration = async filename => {
+  return new Promise((resolve, reject) => {
+    try {
+      getAudioDurationInSeconds(ytmndPath + filename).then(duration => {
+        resolve(duration)
+      })
+    } catch (error) {
+      reject(error)
     }
+  })
+}
+
+// render video to post
+const createTikTok = async (imageFilename, soundFilename) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      let commands = []
+      let length = await getDuration(soundFilename)
+
+      if (length < 5) {
+        length = Math.round(length * 3)
+      } else {
+        length = Math.round(length)
+      }
+
+      console.log("Length: " + length)
+
+      // render audio
+      commands.push(`sox sites/${ytmnd}/${soundFilename} sites/${ytmnd}/sound_looped.mp3 repeat 20 && ffmpeg -ss 0 -t 60 -i sites/${ytmnd}/sound_looped.mp3 sites/${ytmnd}/sound_final.mp3`)
+
+      // resize and stack image
+      commands.push(`convert sites/${ytmnd}/${imageFilename} -resize 375x375 sites/${ytmnd}/resized_${imageFilename}`)
+
+      commands.push(`ffmpeg -i sites/${ytmnd}/resized_${imageFilename} -i sites/${ytmnd}/resized_${imageFilename} -i sites/${ytmnd}/resized_${imageFilename} -filter_complex vstack=inputs=3 sites/${ytmnd}/stacked_${imageFilename}`)
+
+      // composite image and video
+      if(path.extname(imageFilename) == ".gif") {
+        commands.push(`ffmpeg -i assets/bg.mp4 -ignore_loop 0 -i sites/${ytmnd}/stacked_${imageFilename} -filter_complex "[0:v][1:v] overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2:enable='between(t,0,60):shortest=1" -pix_fmt yuv420p -c:a copy sites/${ytmnd}/video.mp4`)
+      } else {
+        commands.push(`ffmpeg -i assets/bg.mp4 -i sites/${ytmnd}/stacked_${imageFilename} -filter_complex "[0:v][1:v] overlay=0:0" -pix_fmt yuv420p -c:a copy sites/${ytmnd}/video.mp4`)
+      }
+
+      // render video with sound
+      commands.push(`ffmpeg -i sites/${ytmnd}/video.mp4 -i sites/${ytmnd}/sound_final.mp3 -filter_complex "[1:0] adelay=0|0 [delayed];[0:1][delayed] amix=inputs=2" -map 0:0 -c:a aac -strict -2 -c:v copy sites/${ytmnd}/video_wsound.mp4`)
+
+      // crop video to specified length
+      commands.push(`ffmpeg -ss 00:00:00 -i sites/${ytmnd}/video_wsound.mp4 -to 00:00:${length} -c copy sites/${ytmnd}/tiktok.mp4`)
+
+      // cleanup
+      // let cleanUp = `rm *_*`
+
+      console.log(commands)
+
+      resolve(commands.join(" && "))
+    } catch (error) {
+      reject(error)
+    }
+  })
+}
+
+const run = async () => {
+  await cmd.run(`rm -rf sites/${ytmnd} && mkdir sites/${ytmnd}`)
+
+  let html = await fetchHtml()
+  ytmndInfo = await getData(html) // global json data from site
+  let imageFilename = ytmndInfo.site.foreground.url.replace(
+    /(.*)\./gi,
+    "image."
   )
+  let soundFilename = ytmndInfo.site.sound.url.replace(/(.*)\./gi, "sound.")
+
+  await download(ytmndInfo.site.foreground.url, imageFilename)
+  await download(ytmndInfo.site.sound.url, soundFilename)
+
+  //  ytmndInfo.duration = await getDuration(soundFilename)
+  let command = await createTikTok(imageFilename, soundFilename)
+
+  await cmd.run(command)
 
   return true
 }
